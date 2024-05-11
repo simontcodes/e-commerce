0c49af93048168182531ac2375c4128e1fb2ba49
simontcodes <simont.codes@gmail.com>
2024-05-11 03:24:55 -0500

    initial set up and custom header

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
+const { EndOfLineState } = require("typescript");
+
 module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', {EndOfLine: 'auto'}],
+    'no-console': 'off',
+  }
 }
